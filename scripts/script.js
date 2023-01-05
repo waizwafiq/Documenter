11296075f108928a5a2cbc497153b056383d3459
Waiz Wafiq <mwaizwafiq@gmail.com>
2023-01-06 05:05:30 +0800

    Added highlighter at right sidebar as the user scrolls down

--- scripts/script.js
+++ scripts/script.js
@@ -28,10 +28,46 @@ function copyCode(button) {
 
     // Deselect the text
     selection.removeAllRanges();
-    
+
     button.textContent = 'Copied!'
 
-    setTimeout(function() {
+    setTimeout(function () {
         button.textContent = 'Copy';
-      }, 1800);
+    }, 1800);
 }
+
+
+// Color the "On This Page" tab when the topic is in view
+const links = document.querySelectorAll(".toc__listitem")
+const topics = document.querySelectorAll(".note__body > div h2, .note__body > div h3")
+
+for (const i of topics)
+    console.log(i.parentElement.offsetHeight)
+
+function currentTopic() {
+
+    const scrollPos = window.scrollY; // current scroll position
+    
+    for (const topic of topics) {
+
+        //get the div of the section
+        const topicSectDiv = topic.parentElement
+
+        // get the top position of the topic/subtopic
+        const topicPos = topicSectDiv.offsetTop;
+        const topicBottom =  (topicPos + topicSectDiv.offsetHeight)
+
+        if (scrollPos > topicPos - 100 && scrollPos < topicBottom - 80) {
+            // if the topic is in view
+            const id = topic.getAttribute("id")
+            const link = document.querySelector(`a[href="#${id}"]`).parentElement
+            link.classList.add("active")
+            console.log('uwu')
+        } else {
+            const id = topic.getAttribute("id")
+            const link = document.querySelector(`a[href="#${id}"]`).parentElement
+            link.classList.remove("active");
+        }
+    }
+}
+window.addEventListener("scroll", currentTopic);
